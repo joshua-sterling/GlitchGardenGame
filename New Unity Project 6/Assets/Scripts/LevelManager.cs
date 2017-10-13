8590a576d01bbe512b63eebf46ecc3cc466c82d4
jsterling15 <joshua.sterling15@gmail.com>
2017-10-12 21:17:05 -0500

    Splash and start screens
    Initial setup of splash and start screens

---
+++ New Unity Project 6/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitLevel()
+    {
+        Debug.Log("Request to quit the game");
+        Application.Quit();
+    }
+
+    public void returnToScene(string name)
+    {
+        SceneManager.LoadScene(name);
+       
+    }
+
+    public void LoadNextLevel()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   
+    
+
+}
