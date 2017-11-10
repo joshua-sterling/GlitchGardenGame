8d9bea9f88cf3c23614dace68d58e61feb5d698e
jsterling15 <joshua.sterling15@gmail.com>
2017-11-10 13:47:19 -0600

    Added Persistent Music Manager
    Added a single persistent music manager that will change music based on scene build order

---
+++ New Unity Project 6/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;   //private audio source
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + name);
+    }
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();   //find the audio source
+
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];            //set the value to a variable
+        Debug.Log("Playing clip: " + levelMusicChangeArray[level]);
+
+        if(thisLevelMusic)  //if there is some music attached
+        {
+            audioSource.clip = thisLevelMusic;                  //tell it what clip to play
+            audioSource.loop = true;                            //loop it
+            audioSource.Play();                                 //play it
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
